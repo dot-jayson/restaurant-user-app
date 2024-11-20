870ee02ad5c09f70c444726325081f130a6ef108
Jayson Chiu <jaysonchiu@Jaysons-MacBook-Air.local>
2024-11-20 15:09:53 +0000

    added markers and list displaying restaurants from db

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
-import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import MapView, { Marker } from "react-native-maps";
 import useLocation from "../../hooks/useLocation";
@@ -8,6 +15,7 @@ import { supabase } from "../../lib/supabase";
 import { Button } from "@rneui/themed";
 
 const Home = () => {
+  const [restaurants, setRestaurants] = useState([]);
   const router = useRouter();
   async function signOut() {
     const { error } = await supabase.auth.signOut();
@@ -16,23 +24,30 @@ const Home = () => {
   }
 
   async function fetchRestaurants() {
-    let { data, error } = await supabase
-      .from("restaurants")
-      .select(
-        "restaurant_name, st_y(location::geometry) as lat, st_x(location::geometry) as long"
-      );
-    console.log(data);
+    const { data, error } = await supabase.rpc("get_restaurants_with_location");
+
     if (error) {
       console.error("Error fetching restaurants:", error);
     }
     if (data && data.length > 0) {
-      data.forEach((restaurant) => {
-        console.log(`Restaurant:${restaurant.restaurant_name} `);
-        console.log(`Location:${restaurant.location} `);
-      });
+      setRestaurants(data);
     } else {
       console.log("No restaurants found");
     }
+  }
+
+  function renderRestaurantItem({ item }) {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          router.push(`/pages/restaurant/${item.restaurant_id}`);
+        }}
+      >
+        <View className="bg-white p-4 m-2 rounded-lg shadow-sm">
+          <Text className="text-lg text-gray-800">{item.restaurant_name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
   }
 
   useEffect(() => {
@@ -54,7 +69,7 @@ const Home = () => {
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   };
-  const londonLocation = {
+  const tempInitialLocation = {
     latitude: 51.5,
     longitude: 0.12,
     latitudeDelta: 0.01,
@@ -67,11 +82,43 @@ const Home = () => {
           <View style={styles.verticallySpaced}>
             <Button title="Sign out" onPress={() => signOut()} />
           </View>
-          <MapView initialRegion={londonLocation} style={styles.map}>
-            <Marker coordinate={londonLocation} title="You are here" />
+          <MapView initialRegion={tempInitialLocation} style={styles.map}>
+            <Marker coordinate={tempInitialLocation} title="You are here">
+              <Image
+                className="w-[50px] h-[50px]"
+                source={require("../../assets/location-marker.png")}
+              />
+            </Marker>
+
+            {restaurants.map((restaurant) => {
+              return (
+                <Marker
+                  key={restaurant.restaurant_id}
+                  coordinate={{
+                    latitude: restaurant.latitude,
+                    longitude: restaurant.longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                  }}
+                  title={restaurant.restaurant_name}
+                  onPress={() => {
+                    router.push(
+                      `/pages/restaurant/${restaurant.restaurant_id}`
+                    );
+                  }}
+                />
+              );
+            })}
           </MapView>
-          <View className="mt-2">
-            <Text className="m-auto">Restaurant List</Text>
+          <View className="flex-1 p-4">
+            <Text className="text-center text-xl font-bold mb-4">
+              Restaurant List
+            </Text>
+            <FlatList
+              data={restaurants}
+              renderItem={renderRestaurantItem}
+              keyExtractor={(item) => item.restaurant_id}
+            />
           </View>
         </View>
       </SafeAreaView>
